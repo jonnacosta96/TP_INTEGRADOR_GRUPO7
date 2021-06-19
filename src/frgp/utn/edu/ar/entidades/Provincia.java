363dee639cdd57296ed3ef714e4e2774e70dbd07
Jonathan Costa <jonna_costa96@hotmail.com>
2021-06-18 23:33:35 -0300

    1) Implementacion de Hibernate en el projecto. 2) Creacion de clases "Cliente" y "Pais". 3) Creacion de tablas en base de datos, por medio de Hibernate.
    Anotacion: El dia de mañana seguire con las relaciones.

---
+++ src/frgp/utn/edu/ar/entidades/Provincia.java
@@ -0,0 +1,60 @@
+package frgp.utn.edu.ar.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Provincias")
+public class Provincia implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="IdProvincia")
+	private int idProvincia;
+	
+	@Column(name="Nombre")
+	private String nombre;
+	
+	@Column(name="EstadoLinea", columnDefinition="Boolean default true")
+	private boolean estadoProvincia;
+	
+	public Provincia() {
+		
+	}
+
+	public int getIdProvincia() {
+		return idProvincia;
+	}
+
+	public void setIdProvincia(int idProvincia) {
+		this.idProvincia = idProvincia;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public boolean isEstadoProvincia() {
+		return estadoProvincia;
+	}
+
+	public void setEstadoProvincia(boolean estadoProvincia) {
+		this.estadoProvincia = estadoProvincia;
+	}
+
+	@Override
+	public String toString() {
+		return "idProvincia=" + idProvincia + ", nombre=" + nombre + ", estadoProvincia=" + estadoProvincia;
+	}
+	
+	
+}
